e450303d650931e0d6bdadfc2c171e161319bb4b
Alexandr Zykov <alexandrz@gmail.com>
2010-08-03 13:15:51 +0200

    Revert "update status API"
    This reverts commit 0c79cf63f4f04f3dc5ac01ec071be0641f1349bf.

--- lib/rvk/status.rb
+++ lib/rvk/status.rb
@@ -2,33 +2,21 @@ module Vkontakte
   module Status
     
     def set_status text
-      url = URI.parse('http://vk.com/wall.php')
+      url = URI.parse('http://vk.com/profile.php')
       request = Net::HTTP::Post.new(url.path)
-      request.set_form_data({
-        'act'      => 'a_post_wall',
-        'message'  => text,
-        'reply_to' => -1
-      }.merge(fetch_hash))
+      request.set_form_data({'setactivity' => text, 'activityhash' => self.fetch_activityhash})
       request['cookie'] = "remixsid=#{self.session}"
 
       Net::HTTP.new(url.host, url.port).start { |http| http.request(request) }
     end
     
-    def fetch_hash
+    def fetch_activityhash
       request = open('http://vk.com/profile.php', 'Cookie' => "remixsid=#{self.session}")
       profile = request.read.to_s
-      if match = profile.match(/postStatus\((\d+), '([^']+)'\)/)
-        { 'to_id' => match[1], 'hash' => decode_hash(match[2]) }
+      if match = profile.match(/<input type='hidden' id='activityhash' value='([^']+)'>/)
+        match[1]
       else
         raise VkontakteError, "Could not find status hash"
-      end
-    end
-    
-    def decode_hash hash
-      a = hash[-5..-1] + hash[4..hash.length-9]
-      a.length.times.inject('') do |all, i|
-        j = a.length - i - 1
-        all + a[j..j]
       end
     end
     
