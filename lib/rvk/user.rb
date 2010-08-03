4c18136fa98dab52ca97e45009ad7b869470a713
Alexandr Zykov <alexandrz@gmail.com>
2010-08-03 13:15:36 +0200

    Revert "update session API"
    This reverts commit ac2823d295997beee0581aadef7fbdaa065d373e.

--- lib/rvk/user.rb
+++ lib/rvk/user.rb
@@ -9,17 +9,18 @@ module Vkontakte
       if (args.size == 1)
         self.session = args.first
       else
-        self.session = self.fetch_session *args
+        self.session = self.fetch_session args
       end      
     end
     
-    def fetch_session email, password
+    def fetch_session args
+      (email, password) = args
       resource = Net::HTTP.post_form(URI.parse('http://login.vk.com/'), 
           { 'email' => email, 'pass' => password, 'vk' => '', 'act' => 'login' })
       if resource.body.empty?
         raise VkontakteError, "Could not fetch session"
       end
-      if match = resource.body.match(/name='s' value='([a-z0-9]+)'/)
+      if match = resource.body.match(/id='s' value='([a-z0-9]+)'/)
         match[1]
       else
         raise VkontakteError, "Could not find session"
