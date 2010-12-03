e21c981440baefa5e709424718f05e8d75a65091
Andrey A.I. Sitnik <andrey@sitnik.ru>
2010-12-04 00:10:01 +0300

    Update status API

--- lib/rvk/status.rb
+++ lib/rvk/status.rb
@@ -3,52 +3,32 @@ module Vkontakte
     def set_status(text)
       request = open('http://vk.com/profile.php', 'Cookie' => "remixsid=#{self.session}")
       @profile = request.read.to_s
-      if @profile =~ /<input type='hidden' id='activityhash'/
-        set_old_status(text)
-      else
-        set_microblog_status(text)
-      end
-    end
-    
-    def set_old_status(text)
-      url = URI.parse('http://vk.com/profile.php')
-      request = Net::HTTP::Post.new(url.path)
-      request.set_form_data({'setactivity' => text, 'activityhash' => self.fetch_activityhash})
-      request['cookie'] = "remixsid=#{self.session}"
-
-      Net::HTTP.new(url.host, url.port).start { |http| http.request(request) }
-    end
-    
-    def fetch_activityhash
-      if match = @profile.match(/<input type='hidden' id='activityhash' value='([^']+)'>/)
-        match[1]
-      else
-        raise VkontakteError, "Could not find status hash"
-      end
+      set_microblog_status(text)
     end
     
     def set_microblog_status(text)
-      url = URI.parse('http://vk.com/wall.php')
+      url = URI.parse('http://vk.com/al_wall.php')
       request = Net::HTTP::Post.new(url.path)
       request.set_form_data({
-        'act'      => 'a_post_wall',
-        'message'  => text,
-        'reply_to' => -1
-      }.merge(fetch_microblog_data))
+        'al'            => 1,
+        'act'           => 'post',
+        'to_id'         => find_in_profile(/"user_id":(\d+)/),
+        'hash'          => find_in_profile(/"post_hash":"([^"]+)"/),
+        'message'       => text,
+        'note_title'    => '',
+        'status_export' => ''
+      })
       request['cookie'] = "remixsid=#{self.session}"
 
       Net::HTTP.new(url.host, url.port).start { |http| http.request(request) }
     end
     
-    def fetch_microblog_data
-      if match = @profile.match(/postStatus\((\d+), '([^']+)'\)/)
-        hash = match[2]
-        decoded_hash = (hash[-5..-1] + hash[4..hash.length-9]).reverse
-        { 'to_id' => match[1], 'hash' => decoded_hash }
+    def find_in_profile(regexp)
+      if match = @profile.match(regexp)
+        match[1]
       else
-        raise VkontakteError, "Could not find status hash"
+        raise VkontakteError, "Could not find status hash or #{regexp}"
       end
     end
-    
   end
 end
